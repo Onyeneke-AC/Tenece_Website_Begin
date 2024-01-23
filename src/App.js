b71cf6ea35b450de8389045818f1d24c7c7c018d
Onyeneke Anthony Chidubem <98026459+Onyeneke-AC@users.noreply.github.com>
2024-01-23 21:00:43 +0100

    Introduction updates

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { BrowserRouter as Router, Routes as Switch , Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
-import Home from './components/pages/Home'
+import Home from './components/pages/Home';
+import Introduction from './components/Introduction';
 
 function App() {
   return (
     <>
       <Router>
         <Navbar />
+        <Introduction />
         <Switch>
           <Route path="/" exact Component={Home}/>
         </Switch>
--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -1,11 +1,25 @@
 import React from 'react'
+// import Button from './Button'
 
 function Introduction() {
   return (
-    <div>
-      
-    </div>
+    <section class="intro content_body" id="home">
+        <div class="write__up">
+            <h2 class="intro__title">
+                Transforming your business with bespoke Technology Solutions
+            </h2>
+            <p class="intro__p">
+                We are a Multi-Competency, Enterprise Transformation & Technology Consulting Firm. We combine expertise and capabilities across many industries and business functions to deliver superior value to our clients.
+            </p>
+            <div class="btn__int">
+            <a href="#intro" class="intro__btn">
+                Read more <span class="rot"><img src="./FontAwesomeicon-caret-right.png" alt="Read more"/></span>
+            </a>
+            </div>
+        </div>
+        <div class="intro__img"><img src="./bgs/first_img.png" alt="Tenece" class="intro__img"/></div>
+    </section>
   )
 }
 
-export default Introduction
+export default Introduction;
