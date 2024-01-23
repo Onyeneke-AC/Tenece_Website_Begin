e8f929c49d2b57b6f7bc80825bbbc5379f98d80a
Onyeneke Anthony Chidubem <98026459+Onyeneke-AC@users.noreply.github.com>
2024-01-23 23:10:16 +0100

    Introduction updates

--- src/components/Button.js
+++ src/components/Button.js
@@ -6,13 +6,13 @@ const STYLES = ['btn--primary', 'btn--outline'] //css classes
 
 const SIZES = ['btn--medium', 'btn--large']
 
-export const Button = ({children, type, onClick, buttonStyle, buttonSize}) => {
+export const Button = ({children, type, onClick, buttonStyle, buttonSize, buttonRoute}) => {
     const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0];
 
     const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
 
     return (
-        <Link to='/sign-up' className='btn-mobile'>
+        <Link to={buttonRoute} className='btn-mobile'>
             <button 
             className={`btn ${checkButtonStyle} ${checkButtonSize}`}
             onClick={onClick}
--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -1,24 +1,24 @@
-import React from 'react'
-// import Button from './Button'
+import React from 'react';
+import { Button } from './Button';
 
 function Introduction() {
   return (
-    <section class="intro content_body" id="home">
-        <div class="write__up">
-            <h2 class="intro__title">
+    <div className="intro" id="home">
+        <div className="write__up">
+            <h2 className="intro__title">
                 Transforming your business with bespoke Technology Solutions
             </h2>
-            <p class="intro__p">
+            <p className="intro__p">
                 We are a Multi-Competency, Enterprise Transformation & Technology Consulting Firm. We combine expertise and capabilities across many industries and business functions to deliver superior value to our clients.
             </p>
-            <div class="btn__int">
-            <a href="#intro" class="intro__btn">
-                Read more <span class="rot"><img src="./FontAwesomeicon-caret-right.png" alt="Read more"/></span>
-            </a>
+            <div className="btn__int">
+                <Button className='btns' buttonStyle='btn--outline' buttonRoute='/about-us' buttonSize='btn--medium'>
+                    Read more <i className="fas fa-caret-right"/>
+                </Button>
             </div>
         </div>
-        <div class="intro__img"><img src="./bgs/first_img.png" alt="Tenece" class="intro__img"/></div>
-    </section>
+        <div className="intro__img"><img src="./bgs/first_img.png" alt="Tenece" className="intro__img"/></div>
+    </div>
   )
 }
 
