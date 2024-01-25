096e0e54c01dd21cf1c92cecc9eaed7b87844648
Onyeneke Anthony Chidubem <98026459+Onyeneke-AC@users.noreply.github.com>
2024-01-25 21:29:16 +0100

    Introduction updates

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import { Button } from './Button';
+import "./Introduction.css";
 
 function Introduction() {
   return (
     <div className="intro" id="home">
-        <div className="write__up">
-            <h2 className="intro__title">
+        <div className="write_up">
+            <h2 className="intro_title">
                 Transforming your business with bespoke Technology Solutions
             </h2>
-            <p className="intro__p">
+            <p className="intro_p">
                 We are a Multi-Competency, Enterprise Transformation & Technology Consulting Firm. We combine expertise and capabilities across many industries and business functions to deliver superior value to our clients.
             </p>
-            <div className="btn__int">
+            <div className="btn_int">
                 <Button className='btns' buttonStyle='btn--outline' buttonRoute='/about-us' buttonSize='btn--medium'>
                     Read more <i className="fas fa-caret-right"/>
                 </Button>
             </div>
         </div>
-        <div className="intro__img"><img src="./bgs/first_img.png" alt="Tenece" className="intro__img"/></div>
+        <div className="intro_img"><img src="./bgs/first_img.png" alt="Tenece" className="img"/></div>
     </div>
   )
 }
