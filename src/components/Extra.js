d5f22d93349fc8e3903debc052a8c13fc8b672fd
Onyeneke Anthony Chidubem <98026459+Onyeneke-AC@users.noreply.github.com>
2024-01-26 09:03:14 +0100

    Extra updates

--- src/components/Extra.js
+++ src/components/Extra.js
@@ -4,7 +4,7 @@ import "./Extra.css";
 function Extra() {
   return (
     <div className='extra'>
-      <h2 className="about__h2">
+      <h2 className="about_h2">
             "Tenece is the leading Technology Consulting Services firm in Africa. We provide cutting edge solutions for your business"
         </h2>
         <p>
