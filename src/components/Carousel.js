dcee40c79fe14b52673ae8a1c45af9552cecf315
Onyeneke Anthony Chidubem <98026459+Onyeneke-AC@users.noreply.github.com>
2024-02-04 20:31:01 +0100

    git check complete

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const Carousel  = () => {
     return (
         <div className="carousel">
-            Something is wrong
+            
         </div>
     )
 };
