0daa477e61f8f34c4c4218c13ce45001fe5c203f
Onyeneke Anthony Chidubem <98026459+Onyeneke-AC@users.noreply.github.com>
2024-02-06 21:03:38 +0100

    Partner carousel updates

--- src/components/ParCliCarousel.js
+++ src/components/ParCliCarousel.js
@@ -2,9 +2,9 @@ import React from 'react';
 import "./Partners.css"
 
 export const ParCliCarousel  = ({ item }) => {
-    return (
-            <div className='carousel_sec'>    
-                <img src={item.src} alt={item.alt} className='carousel_img'/>
-            </div>
+    return (   
+        
+        <img src={item.src} alt={item.alt} className='carousel_img'/>
+        
     )
 };
