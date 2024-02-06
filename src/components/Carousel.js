d5e0ef55ec4b1e5eaa4abcf5b04ff92c047784d2
Onyeneke Anthony Chidubem <98026459+Onyeneke-AC@users.noreply.github.com>
2024-02-06 11:58:07 +0100

    News with Carousel Slider completed

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import "./Carousel.css";
 
 export const Carousel  = ({ item }) => {
     return (
-        <div className="carousel">
-
-        </div>
+        <Link to={item.path} className="carousel_link">
+            <div className="carousel"> { item.news } </div>
+        </Link>
     )
 };
--- src/components/News.js
+++ src/components/News.js
@@ -4,16 +4,29 @@ import { NewsData } from './NewsData';
 import './News.css';
 
 function News() {
-  return (
-    <div className='news_section'>
+    
+    return (
+        <div className='news_section'>
       <div className="news">
         NEWS
       </div>
+      <div className='news_slide'>
       <div className="news_carousel">
-        {NewsData.map((item, index) => {
-            return <Carousel item={item} key={index} className='news_slide' carouselSize= 'size--primary' carouselFont='font--primary'/>
-        })
+        <div className='news_exact'>
+            {NewsData.map((item, index) => {
+            return( 
+                <Carousel item={item} key={index} />
+                )
+            })
         }
+        {NewsData.map((item, index) => {
+            return( 
+                <Carousel item={item} key={index} />
+                )
+            })
+        }
+        </div>
+      </div>
       </div>
     </div>
   )
