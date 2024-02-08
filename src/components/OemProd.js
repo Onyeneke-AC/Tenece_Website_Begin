0690af9220cddd570c94bc15e991655da53355a4
Onyeneke Anthony Chidubem <98026459+Onyeneke-AC@users.noreply.github.com>
2024-02-08 22:16:43 +0100

    OEM and Prop Products sections begin

---
+++ src/components/OemProd.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function OemProd() {
+  return (
+    <div className='prop-oem'>
+      <h2 className="sub_h2">
+        OEM products
+      </h2>
+      <p>
+        Partner Solutions we offer
+      </p>
+    </div>
+  )
+}
+
+export default OemProd
---
+++ src/components/PropProd.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function PropProd() {
+  return (
+    <div className='prop-oem'>
+      <h2 className="sub_h2">
+        OEM products
+      </h2>
+      <p>
+        Solutions Developed by Tenece
+      </p>
+    </div>
+  )
+}
+
+export default PropProd
