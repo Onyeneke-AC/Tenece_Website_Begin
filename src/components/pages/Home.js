5d16b7c4560b61fab4f22a18cd964cd4aaf83363
Onyeneke Anthony Chidubem <98026459+Onyeneke-AC@users.noreply.github.com>
2024-02-10 23:50:09 +0100

    Finalizing home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../../App.css';
+// import '../../App.css';
 import Navbar from '../../components/Navbar';
 import Introduction from '../../components/Introduction';
 import Extra from '../../components/Extra';
