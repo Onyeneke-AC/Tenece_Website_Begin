d5233854b7cd8d6b0e93ab29e88cf26b0f994afd
Onyeneke Anthony Chidubem <98026459+Onyeneke-AC@users.noreply.github.com>
2024-02-10 23:43:17 +0100

    Home page import into App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes as Switch , Route } from 'react-router-dom';
 import './App.css';
+import Home from './components/pages/Home';
 
 function App() {
 
