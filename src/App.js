0e951377ed7e5e6072984ef109d6fbe18c654e1e
Onyeneke Anthony Chidubem <98026459+Onyeneke-AC@users.noreply.github.com>
2024-02-10 23:42:14 +0100

    Moving components from App.js to Home page

--- src/App.js
+++ src/App.js
@@ -1,35 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes as Switch , Route } from 'react-router-dom';
 import './App.css';
-import Navbar from './components/Navbar';
-import Home from './components/pages/Home';
-import Introduction from './components/Introduction';
-import Extra from './components/Extra';
-import Footer from './components/Footer';
-import Sidebar from './components/Sidebar';
-import News from './components/News';
-import Partners from './components/Partners';
-import Clients from './components/Clients';
-import Services from './components/Services';
-import PropProd from './components/PropProd';
-import OemProd from './components/OemProd';
 
 function App() {
 
   return (
     <>
       <Router>
-        <Navbar />
-        <Sidebar />
-        <News />
-        <Introduction />
-        <Extra />
-        <Services />
-        <PropProd />
-        <OemProd />
-        <Clients />
-        <Partners />
-        <Footer />
         <Switch>
           <Route path="/" exact Component={Home}/>
         </Switch>
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import '../../App.css';
-// import Footer from '../Footer';
+import Navbar from '../../components/Navbar';
+import Introduction from '../../components/Introduction';
+import Extra from '../../components/Extra';
+import Footer from '../../components/Footer';
+import Sidebar from '../../components/Sidebar';
+import News from '../../components/News';
+import Partners from '../../components/Partners';
+import Clients from '../../components/Clients';
+import Services from '../../components/Services';
+import PropProd from '../../components/PropProd';
+import OemProd from '../../components/OemProd';
 
 function Home() {
   return (
     <>
-      {/* <Footer /> */}
+      <Navbar />
+      <Sidebar />
+      <News />
+      <Introduction />
+      <Extra />
+      <Services />
+      <PropProd />
+      <OemProd />
+      <Clients />
+      <Partners />
+      <Footer />
     </>
   );
 }
